b6d0fc37491fbab8f3b97aba431fd62efab5b035
Naides <iancanlas0125@gmail.com>
2025-10-20 14:55:24 +0800

    Route Changes

--- ian-port/src/App.jsx
+++ ian-port/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar'
 import About from './Pages/About'
 import AboutPage from './Pages/AboutPage'
@@ -9,18 +9,22 @@ import ContactPage from './Pages/ContactPage'
 
 const App = () => {
   return (
-    <div>
-      <Router basename="/ian-port">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<About />} />
-          <Route path="/aboutPage" element={<AboutPage />} />
-          <Route path="/worksPage" element={<WorksPage />} />
-          <Route path="/projectsPage" element={<ProjectsPage />} />
-          <Route path="/contactPage" element={<ContactPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router basename="/ian-port">
+      <Navbar />
+      <Routes>
+
+        <Route path="/" element={<About />} />
+
+
+        <Route path="/aboutPage" element={<AboutPage />} />
+        <Route path="/worksPage" element={<WorksPage />} />
+        <Route path="/projectsPage" element={<ProjectsPage />} />
+        <Route path="/contactPage" element={<ContactPage />} />
+
+        
+        <Route path="*" element={<About />} />
+      </Routes>
+    </Router>
   )
 }
 
